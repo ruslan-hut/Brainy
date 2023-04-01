4ce17e23df9c0219e8fe1739aa93e783a84ec822
ruslanhut <dev@programmer.com.ua>
2023-04-01 10:45:27 +0200

    Initial commit

---
+++ core/config.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"sync"
+)
+
+type Config struct {
+	TelegramApiKey string `yaml:"telegram_api_key" env-default:""`
+	OpenAIApiKey   string `yaml:"openai_api_key" env-default:""`
+	Mongo          struct {
+		Enabled  bool   `yaml:"enabled" env-default:"false"`
+		Host     string `yaml:"host" env-default:"127.0.0.1"`
+		Port     string `yaml:"port" env-default:"27017"`
+		User     string `yaml:"user" env-default:"admin"`
+		Password string `yaml:"password" env-default:"pass"`
+		Database string `yaml:"database" env-default:""`
+	}
+}
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() (*Config, error) {
+	var err error
+	once.Do(func() {
+		instance = &Config{}
+		if err = cleanenv.ReadConfig("config.yml", instance); err != nil {
+			desc, _ := cleanenv.GetDescription(instance, nil)
+			err = fmt.Errorf("config: %s; %s", err, desc)
+			instance = nil
+		}
+	})
+	return instance, err
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module AslamistBot
+
+go 1.20
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.4.2
+)
+
+require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
